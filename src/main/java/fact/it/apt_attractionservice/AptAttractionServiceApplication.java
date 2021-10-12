8373b1b15d517bd85f614ad85172372ef8823dce
Ferre Vangenechten <r0794129@student.thomasmore.be>
2021-10-12 14:31:00 +0200

    add project

---
+++ src/main/java/fact/it/apt_attractionservice/AptAttractionServiceApplication.java
@@ -0,0 +1,13 @@
+package fact.it.apt_attractionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptAttractionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AptAttractionServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fact/it/apt_attractionservice/AptAttractionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.apt_attractionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptAttractionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
